ec8438ff642b828cdd6ee903687ca7bfd03b7eac
koyeary <katyeary@gmail.com>
2020-08-10 15:50:26 -0700

    Initial commit: import the app architecture from UCLA repo and create the webpack.config file

---
+++ Develop/webpack.config.js
@@ -0,0 +1,62 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./assets/js/index.js",
+    favorites: "./assets/js/favorites.js",
+    topic: "./assets/js/topic.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget Tracker App",
+      short_name: "Budget Tracker App",
+      description: "An application that allows you to track your spending both on- and offline.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("assets/images/icons/android-chrome-192x192.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+
+
+module.exports = config;
