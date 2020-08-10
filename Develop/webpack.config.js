d2938debc3cb894b1ef2b054683805fa6f2390fb
koyeary <katyeary@gmail.com>
2020-08-10 16:32:47 -0700

    Commit package.json to include updated devDependencies - i.e. babel and webpack

--- Develop/webpack.config.js
+++ Develop/webpack.config.js
@@ -2,10 +2,11 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
+  mode: "production",
   entry: {
-    app: "./assets/js/index.js",
-    favorites: "./assets/js/favorites.js",
-    topic: "./assets/js/topic.js"
+    app: "./public/index.js",
+  /*   favorites: "./assets/js/favorites.js",
+    topic: "./assets/js/topic.js" */
   },
   output: {
     path: __dirname + "/dist",
@@ -33,13 +34,8 @@ const config = {
       description: "An application that allows you to track your spending both on- and offline.",
       background_color: "#01579b",
       theme_color: "#ffffff",
-      "theme-color": "#ffffff",
       start_url: "/",
-      icons: [{
-        src: path.resolve("assets/images/icons/android-chrome-192x192.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
-        destination: path.join("assets", "icons")
-      }]
+      display: "standalone"
     })
   ],
   module: {
